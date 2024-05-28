3506834784422b97e4fea82563d2488f1b3db10f
Leslie <lesliekanderson@fastmail.com>
2024-05-27 21:35:30 -0400

    test with tables already created

--- Backend/data_to_bq.go
+++ Backend/data_to_bq.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
 
 	"cloud.google.com/go/bigquery"
 )
@@ -25,62 +24,62 @@ func chunkData(data []recidData, chunkSize int) [][]recidData {
 	return chunks
 }
 
-func createOrCheckTable(ctx context.Context, client *bigquery.Client, datasetID, tableID string) error {
-	// Check if the table exists
-	_, err := client.Dataset(datasetID).Table(tableID).Metadata(ctx)
-	if err != nil {
-		fmt.Println("Table doesn't exist. Creating it.")
-		// If the table doesn't exist, create it
-		if _, ok := err.(*bigquery.Error); ok {
+// func createOrCheckTable(ctx context.Context, client *bigquery.Client, datasetID, tableID string) error {
+// 	// Check if the table exists
+// 	_, err := client.Dataset(datasetID).Table(tableID).Metadata(ctx)
+// 	if err != nil {
+// 		fmt.Println("Table doesn't exist. Creating it.")
+// 		// If the table doesn't exist, create it
+// 		if _, ok := err.(*bigquery.Error); ok {
 
-			// schema := "schema.json"
-			// schemaData, err := os.ReadFile(schema)
-			// if err != nil {
-			// 	log.Fatalf("Failed to read schema file: %v", err)
-			// }
+// 			// schema := "schema.json"
+// 			// schemaData, err := os.ReadFile(schema)
+// 			// if err != nil {
+// 			// 	log.Fatalf("Failed to read schema file: %v", err)
+// 			// }
 
-			// var schemaFields []tableSchema
-			// if err := json.Unmarshal(schemaData, &schemaFields); err != nil {
-			// 	log.Fatalf("Failed to unmarshal schema JSON: %v", err)
-			// }
+// 			// var schemaFields []tableSchema
+// 			// if err := json.Unmarshal(schemaData, &schemaFields); err != nil {
+// 			// 	log.Fatalf("Failed to unmarshal schema JSON: %v", err)
+// 			// }
 
-			// // Convert the schema fields to BigQuery table schema
-			// var tableSchema bigquery.Schema
-			// for _, field := range schemaFields {
-			// 	fieldSchema := &bigquery.FieldSchema{
-			// 		Name: field.Name,
-			// 		Type: bigquery.FieldType(field.Type),
-			// 	}
-			// 	// Set the mode (REQUIRED, NULLABLE, REPEATED)
-			// 	if field.Mode == "REQUIRED" {
-			// 		fieldSchema.Required = true
-			// 	} else if field.Mode == "REPEATED" {
-			// 		fieldSchema.Repeated = true
-			// 	} // BigQuery FieldSchema is NULLABLE by default if mode is not set
+// 			// // Convert the schema fields to BigQuery table schema
+// 			// var tableSchema bigquery.Schema
+// 			// for _, field := range schemaFields {
+// 			// 	fieldSchema := &bigquery.FieldSchema{
+// 			// 		Name: field.Name,
+// 			// 		Type: bigquery.FieldType(field.Type),
+// 			// 	}
+// 			// 	// Set the mode (REQUIRED, NULLABLE, REPEATED)
+// 			// 	if field.Mode == "REQUIRED" {
+// 			// 		fieldSchema.Required = true
+// 			// 	} else if field.Mode == "REPEATED" {
+// 			// 		fieldSchema.Repeated = true
+// 			// 	} // BigQuery FieldSchema is NULLABLE by default if mode is not set
 
-			// 	tableSchema = append(tableSchema, fieldSchema)
-			// }
+// 			// 	tableSchema = append(tableSchema, fieldSchema)
+// 			// }
 
-			tableSchema, err := bigquery.InferSchema(recidData{})
-			if err != nil {
-				log.Fatalf("Failed to infer schema: %v", err)
-			}
-			table := client.Dataset(datasetID).Table(tableID)
-			if err := table.Create(ctx, &bigquery.TableMetadata{
-				Schema: tableSchema,
-			}); err != nil {
-				log.Fatalf("Failed to create test table: %v", err)
-			}
+// 			tableSchema, err := bigquery.InferSchema(recidData{})
+// 			if err != nil {
+// 				log.Fatalf("Failed to infer schema: %v", err)
+// 			}
+// 			table := client.Dataset(datasetID).Table(tableID)
+// 			if err := table.Create(ctx, &bigquery.TableMetadata{
+// 				Schema: tableSchema,
+// 			}); err != nil {
+// 				log.Fatalf("Failed to create test table: %v", err)
+// 			}
 
-			log.Printf("Table %s:%s has been created.", datasetID, tableID)
-			return nil
-		}
-		return err
-	}
+// 			log.Printf("Table %s:%s has been created.", datasetID, tableID)
+// 			return nil
+// 		}
+// 		return err
+// 	}
 
-	log.Printf("Table %s:%s already exists.", datasetID, tableID)
-	return nil
-}
+// 	log.Printf("Table %s:%s already exists.", datasetID, tableID)
+// 	return nil
+// }
 
 func writeToBQ(trainSet []recidData, testSet []recidData) error {
 	projectID := "msds434-finalproj"
@@ -95,20 +94,19 @@ func writeToBQ(trainSet []recidData, testSet []recidData) error {
 
 	// Write train table
 	trainTableID := "train_recid"
-	err = createOrCheckTable(ctx, client, datasetID, trainTableID)
-	if err != nil {
-		log.Fatalf("Failed to create or check table: %v", err)
-	}
-
-	time.Sleep(5 * time.Second)
+	//err = createOrCheckTable(ctx, client, datasetID, trainTableID)
+	// if err != nil {
+	// 	log.Fatalf("Failed to create or check table: %v", err)
+	// }
 
 	// Insert data into train table
-	u_train := client.Dataset(datasetID).Table(trainTableID).Uploader()
+	up_train := client.Dataset(datasetID).Table(trainTableID).Uploader()
+
 	// Chunk data and upload each chunk
 	chunkSize := 500 // Adjust the chunk size as needed
 	chunks := chunkData(trainSet, chunkSize)
 	for _, chunk := range chunks {
-		if err := u_train.Put(ctx, chunk); err != nil {
+		if err := up_train.Put(ctx, chunk); err != nil {
 			log.Fatalf("Failed to insert chunk into train table: %v", err)
 		}
 	}
@@ -124,16 +122,16 @@ func writeToBQ(trainSet []recidData, testSet []recidData) error {
 	// Write test table
 	testTableID := "test_recid"
 
-	err = createOrCheckTable(ctx, client, datasetID, testTableID)
-	if err != nil {
-		log.Fatalf("Failed to create or check table: %v", err)
-	}
+	//err = createOrCheckTable(ctx, client, datasetID, testTableID)
+	// if err != nil {
+	// 	log.Fatalf("Failed to create or check table: %v", err)
+	// }
 
-	time.Sleep(5 * time.Second)
+	// time.Sleep(5 * time.Second)
 
 	// Insert data into test table
-	u_test := client.Dataset(datasetID).Table(testTableID).Uploader()
-	if err := u_test.Put(ctx, testSet); err != nil {
+	up_test := client.Dataset(datasetID).Table(testTableID).Uploader()
+	if err := up_test.Put(ctx, testSet); err != nil {
 		log.Fatalf("Failed to insert data into test table: %v", err)
 	}
 
