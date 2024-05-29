c70b69d1912c91f518d73f651393995d1a38e206
Leslie <lesliekanderson@fastmail.com>
2024-05-29 14:06:00 -0400

    update html to all user to enter id to search

--- Frontend/handlers.go
+++ Frontend/handlers.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 	"text/template"
 )
 
@@ -23,11 +22,11 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 
 // GetPredictionHandler is used to get the prediction result for a given ID
 func getPredictionHandler(w http.ResponseWriter, r *http.Request) {
-	paramStr := strings.Split(r.URL.Path, "/")
-
-	recidID := paramStr[len(paramStr)-1]
-
+	// paramStr := strings.Split(r.URL.Path, "/")
+	// recidID := paramStr[len(paramStr)-1]
 	//fmt.Fprintf(w, "Prediction results for %s:\n", recidID)
+
+	recidID := r.URL.Query().Get("recidID")
 	predictions, err := predictQuery(recidID)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error fetching prediction results: %v", err), http.StatusInternalServerError)
