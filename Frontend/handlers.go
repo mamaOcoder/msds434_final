285838b441766023207413ec28acaabce4d42caa
Leslie <lesliekanderson@fastmail.com>
2024-05-29 14:49:17 -0400

    update handlers

--- Frontend/handlers.go
+++ Frontend/handlers.go
@@ -27,15 +27,12 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
 // GetPredictionHandler is used to get the prediction result for a given ID
 func getPredictionHandler(w http.ResponseWriter, r *http.Request) {
-	// paramStr := strings.Split(r.URL.Path, "/")
-	// recidID := paramStr[len(paramStr)-1]
-	//fmt.Fprintf(w, "Prediction results for %s:\n", recidID)
-
 	recidID := r.URL.Query().Get("recidID")
 	if recidID == "" {
-		http.Error(w, "Missing recidivism ID parameter", http.StatusBadRequest)
+		http.Error(w, "Missing recidID parameter", http.StatusBadRequest)
 		return
 	}
+
 	predictions, err := predictQuery(recidID)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error fetching prediction results: %v", err), http.StatusInternalServerError)
@@ -44,13 +41,12 @@ func getPredictionHandler(w http.ResponseWriter, r *http.Request) {
 
 	tmpl, err := template.ParseFiles("results.html")
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Error parsing HTML template: %v", err), http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("Error loading template: %v", err), http.StatusInternalServerError)
 		return
 	}
 
 	err = tmpl.Execute(w, predictions)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
 }
